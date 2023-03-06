9562712e76ef6ebacfc70973d184ec67b5fc0ea8
danie <daniel.vikulov@yandex.ru>
2023-03-06 13:46:13 +0300

    def-fluid initial commit with sqrt

--- src/core/maths/Operator.h
+++ src/core/maths/Operator.h
@@ -17,15 +17,20 @@
 
 namespace unreal_fluid::math {
 
-    template<typename T>
-    double inverseSqrt(T x) {
-        //TODO make pure double sqrt calculation
-        auto temp = static_cast<float>(x);
-        int i = *(int*)&temp;
-        i = 0x5f3759df - (i >> 1);
-        float y = *(float*)&i;
-        y *= (1.5F - 0.5F * x * y * y);
-        return y * (1.5F - 0.5F * x * y * y);
-    }
+  template<typename T>
+  double inverseSqrt(T x) {
+    //TODO make pure double sqrt calculation
+    auto temp = static_cast<float>(x);
+    int i = *(int*)&temp;
+    i = 0x5f3759df - (i >> 1);
+    float y = *(float*)&i;
+    y *= (1.5F - 0.5F * x * y * y);
+    return y * (1.5F - 0.5F * x * y * y);
+  }
+
+  template<typename T>
+  double sqrt(T x){
+    return 1.0/ inverseSqrt(x);
+  }
 
 } // namespace maths
